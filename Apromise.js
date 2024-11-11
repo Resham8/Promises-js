96dd59688f27d47bbb87d7eccebdc320868dd5a7
Resham <reshamupadhyay48@gmail.com>
2024-11-11 19:49:24 +0530

    Update Apromise.js

--- Apromise.js
+++ Apromise.js
@@ -1,3 +1,38 @@
-class aPromise {
-    
+const STATE = {
+    FULFILLED : 'fulfilled',
+    REJECTED: 'rejected',
+    PENDING: 'pending'
 }
+class aPromise {
+  #thenCbs = [];
+  #state = STATE.PENDING;
+  #value;
+
+  constructor(cb) {
+    try {
+      cb(this.#onSuccess, this.#onFail);
+    } catch (error) {
+      this.#onFail(error);
+    }
+  }
+
+  #onSuccess(value) {
+    if (this.#state !== STATE.PENDING) return;
+    this.#value = value;
+    this.#state = STATE.FULFILLED;
+  }
+
+  #onFail(value) {
+    if (this.#state !== STATE.PENDING) return;
+    this.#value = value;
+    this.#state = STATE.REJECTED;
+  }
+
+  then(cb) {
+    this.#thenCbs.push(cb);
+  }
+}
+
+module.exports = aPromise;
+
+const p = new Promise(cb);
