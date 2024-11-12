fc85b84f3ca184b7f1f01e9fb2993523d5368b97
Resham <reshamupadhyay48@gmail.com>
2024-11-12 13:02:22 +0530

    addedd  run cbs and then for the promises

--- Apromise.js
+++ Apromise.js
@@ -5,6 +5,7 @@ const STATE = {
 }
 class aPromise {
   #thenCbs = [];
+  #catchCbs = [];
   #state = STATE.PENDING;
   #value;
 
@@ -16,23 +17,45 @@ class aPromise {
     }
   }
 
+  #runCallbacks() {
+    if (this.#state === STATE.FULFILLED) {
+      this.#thenCbs.forEach(callback => {
+        callback(this.#value)
+      })
+
+      this.#thenCbs = [];
+    }
+
+    if (this.#state === STATE.REJECTED) {
+      this.#catchCbs.forEach(callback => {
+        callback(this.#value)
+      })
+
+      this.#catchCbs = [];
+    }
+
+  }
+
   #onSuccess(value) {
     if (this.#state !== STATE.PENDING) return;
     this.#value = value;
     this.#state = STATE.FULFILLED;
+    this.#runCallbacks();
   }
 
   #onFail(value) {
     if (this.#state !== STATE.PENDING) return;
     this.#value = value;
     this.#state = STATE.REJECTED;
+    this.#runCallbacks();
   }
 
   then(cb) {
     this.#thenCbs.push(cb);
+    return this;
   }
 }
 
 module.exports = aPromise;
 
-const p = new Promise(cb);
+// const p = new Promise(cb);
